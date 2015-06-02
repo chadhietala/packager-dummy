31505c06df9f3fb990cfc7a52eb27fa1736ba43d
Chad Hietala <chadhietala@gmail.com>
2015-06-02 12:29:57 -0700

    Building app

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,7 @@
 /* global require, module */
 
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-
-var app = new EmberApp();
+var Packager = require('ember-cli-packager');
+var packager = new Packager();
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -17,4 +16,4 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+module.exports = packager.package();
